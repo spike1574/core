6ab74933489b0bfb018d291a90c6f7fe822096ee
spike <spike1574@naver.com>
2023-04-19 16:55:51 +0900

    23강까지

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,34 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+    @Test
+    void vip_o(){
+
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    void vip_x(){
+
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
