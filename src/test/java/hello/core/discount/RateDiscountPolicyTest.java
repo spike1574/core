9ead3ef2f75a462dd78ca3cf26956ecd4f7238f4
spike <spike1574@naver.com>
2023-04-25 14:09:21 +0900

    40강까지

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -5,8 +5,6 @@ import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RateDiscountPolicyTest {
 
     RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
